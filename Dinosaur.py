58a6e057ec9c125e031b49ded36a259f5074dcd3
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-09-26 08:28:05 -0700

    added methods under dino and robo

--- Dinosaur.py
+++ Dinosaur.py
@@ -3,3 +3,5 @@ class Dinosaur:
         self.name = name
         self.health = 100
         self.attack_power = attack_power
+    def attack(self, robot):
+        robot.health - self.attack_power
--- Robot.py
+++ Robot.py
@@ -6,4 +6,6 @@ class Robot:
         self.name = name
         self.health = 100
         self.active_weapon = Weapon()
-        
+    def attack(self, dinosaur):
+        dinosaur.health - self.active_weapon.attack_power
+
