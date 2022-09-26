3b40e23f3c75c7640ef55597117fe40eac1c5f68
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-09-26 09:02:18 -0700

    Created the battlefield methods to execute the game and logistics

--- Battlefield.py
+++ Battlefield.py
@@ -6,4 +6,29 @@ class Battlefield:
     def __init__(self):
         self.robot = Robot()
         self.dinosaur = Dinosaur()
-        
+    
+    def run_game(self):
+        Battlefield.display_greeting()
+        Battlefield.battle_phase()
+
+    def display_greeting():
+        print("Welcome to the Past Vs the Future! \nOnly one can be left standing")
+
+    def battle_phase(self):
+        while (self.dinosaur.health<=0) or (self.robot.health<=0):    
+            self.robot.health = (self.robot.health - self.dinosaur.attack_power)
+            print(f"{self.dinosaur} attacked {self.robot} dealing {self.dinosaur.attack_power}!")
+            print(f"{self.robot} has {self.robot.health} health remaining!")
+            print()
+            self.dinosaur.health = (self.dinosaur.health - self.robot.active_weapon.attack_power)
+            print(f"{self.robot} attacked {self.dinosaur} dealing {self.robot.active_weapon.attack_power}!")
+            print(f"{self.dinosaur} has {self.dinosaur.health} health remaining!")
+            Battlefield.display_winner()
+
+    def display_winner(self):
+        if self.dinosaur.health <= 0:
+            print("The battle of the ages has concluded! Robot has won!")
+        if self.robot.health <= 0:
+            print("The battle of the ages has concluded! Dinosaur has won!")
+
+            
